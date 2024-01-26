cbcbbb574cf00168149b165176244cc430af777a
wyvernbw <iamwyvern511@gmail.com>
2024-01-26 10:45:01 +0200

    fix: use non pooling url in prisma schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const lol = api.post.hello.useQuery({ text: "my nuts" });
   const allPosts = api.post.getAll.useQuery();
 
   if (allPosts.isLoading) return <div>Loading...</div>;
+
+  if (allPosts.isError) return <div>{allPosts.error.message}</div>;
 
   return <main>{allPosts.data?.map((el) => el.name)}</main>;
 }
