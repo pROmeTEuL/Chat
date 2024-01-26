e51c0323b1beb9d5feed9b8496721462578e9281
wyvernbw <iamwyvern511@gmail.com>
2024-01-26 10:49:51 +0200

    init: shadcn/ui and font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,26 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "~/lib/utils";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={cn(
+        "bg-background min-h-screen font-sans antialiased",
+        fontSans.variable,
+      )}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default api.withTRPC(MyApp);
