e1b18da8d42d97f85882be4367550b630c50e625
wyvernbw <iamwyvern511@gmail.com>
2024-01-26 16:10:59 +0200

    fix: add await in user

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -5,18 +5,20 @@ export const userRouter = createTRPCRouter({
   authenticate: publicProcedure
     .input(z.object({ id: z.string(), name: z.string(), email: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.user.findUnique({ where: { id: input.id } }).then((user) => {
-        if (user) {
-          return user;
-        }
-        return ctx.db.user.create({
-          data: {
-            id: input.id,
-            name: input.name,
-            email: input.email,
-          },
+      await ctx.db.user
+        .findUnique({ where: { id: input.id } })
+        .then(async (user) => {
+          if (user) {
+            return user;
+          }
+          return await ctx.db.user.create({
+            data: {
+              id: input.id,
+              name: input.name,
+              email: input.email,
+            },
+          });
         });
-      });
     }),
   getById: publicProcedure
     .input(z.object({ id: z.string() }))
